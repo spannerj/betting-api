465781f43574da27d35136ae8b2182a015875b3e
SimonChapman <simon.chapman@landregistry.gov.uk>
2016-07-21 17:42:29 +0100

    Add support for trace id

--- flask_skeleton_api/app.py
+++ flask_skeleton_api/app.py
@@ -1,5 +1,21 @@
-from flask import Flask
+from flask import Flask, g, request
+import uuid
+import requests
 
 app = Flask(__name__)
 
 app.config.from_pyfile("config.py")
+
+
+@app.before_request
+def before_request():
+    # Sets the transaction trace id into the global object if it has been provided in the HTTP header from the caller.
+    # Generate a new one if it has not.
+    # We will use this in log messages and also will pass it to other APIs called via requests.
+    trace_id = request.headers.get('X-Trace-ID', None)
+    if trace_id is None:
+        trace_id = uuid.uuid4().hex
+    g.trace_id = trace_id
+    # We also create a session-level requests object for the app to use with the header pre-set, so other APIs will receive it.
+    g.requests = requests.Session()
+    g.requests.headers.update({'X-Trace-ID':trace_id})
