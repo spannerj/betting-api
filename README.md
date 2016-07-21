9aac97e6d957679de8ea3e1e082f8ca5812817fd
SimonChapman <simon.chapman@landregistry.gov.uk>
2016-07-21 17:40:58 +0100

    Update README and tweak health route to use Response object

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 This repository contains a flask application structured in the way that all
 Land Registry flask APIs should be structured going forwards.
 
+You can use this to create your own app.
+Take a copy of all the files, and change all occurences of flask-skeleton-api/flask_skeleton_api to your app name. There will be other places to tweak too such as the exposed port in docker-compose-fragment, so please look through every file before starting to extend it for your own use. 
+
 ## Quick start
 
 ```shell
--- flask_skeleton_api/views/general.py
+++ flask_skeleton_api/views/general.py
@@ -1,6 +1,7 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, Response
 from flask_skeleton_api.app import app
 import json
+from flask import g
 
 # This is the blueprint object that gets registered into the app in blueprints.py.
 general = Blueprint('general', __name__)
@@ -8,8 +9,10 @@ general = Blueprint('general', __name__)
 
 @general.route("/health")
 def check_status():
-    return json.dumps({
+    return Response(response=json.dumps({
         "Status": "OK",
         "headers": str(request.headers),
         "commit": app.config["COMMIT"]
-    })
+    }),  mimetype='application/json', status=200)
+
+
